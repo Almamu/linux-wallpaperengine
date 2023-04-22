21bcb7008f2c6e17ff9f593da0cd2d1e1d74ccbe
Alexis Maiquez <almamu@almamu.com>
2023-04-22 14:02:25 +0200

    Prevent crashing on the PulseAudioPlayingDetector
    Signed-off-by: Alexis Maiquez <almamu@almamu.com>

--- src/WallpaperEngine/Audio/Drivers/Detectors/CPulseAudioPlayingDetector.cpp
+++ src/WallpaperEngine/Audio/Drivers/Detectors/CPulseAudioPlayingDetector.cpp
@@ -18,7 +18,7 @@ namespace WallpaperEngine::Audio::Drivers::Detectors
         // get processid
         const char* value = pa_proplist_gets (info->proplist, PA_PROP_APPLICATION_PROCESS_ID);
 
-        if (atoi (value) != getpid () && pa_cvolume_avg (&info->volume) != PA_VOLUME_MUTED)
+        if (value && atoi (value) != getpid () && pa_cvolume_avg (&info->volume) != PA_VOLUME_MUTED)
             detector->setIsPlaying (true);
     }
 
