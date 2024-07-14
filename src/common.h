94f382dcd2bb50bf8e3be950badb984f57839303
Almamu <wpengine@alma.mu>
2024-07-14 03:57:03 +0200

    fix: build on old ffmpeg versions

--- src/common.h
+++ src/common.h
@@ -3,3 +3,7 @@
 #include "WallpaperEngine/Core/Wallpapers/CWeb.h"
 #include "WallpaperEngine/Logging/CLog.h"
 
+// TODO: FIND A BETTER PLACE TO DO THIS? OLD_API MIGHT EXIST BUT THIS DEFINE MIGHT NOT BE DEFINED...
+#ifndef FF_API_FIFO_OLD_API
+#define 	FF_API_FIFO_OLD_API   (LIBAVUTIL_VERSION_MAJOR < 59)
+#endif
