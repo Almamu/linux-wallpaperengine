cb0df539d43529140361ca3d120b0af6f5ce7021
Almamu <wpengine@alma.mu>
2025-05-10 18:17:17 +0200

    chore: add the reversing tools I have locally

---
+++ tools/reversing/d3dcompiler_47/d3dcompiler_47_shim/pch.h
@@ -0,0 +1,13 @@
+// pch.h: este es un archivo de encabezado precompilado.
+// Los archivos que se muestran a continuación se compilan solo una vez, lo que mejora el rendimiento de la compilación en futuras compilaciones.
+// Esto también afecta al rendimiento de IntelliSense, incluida la integridad del código y muchas funciones de exploración del código.
+// Sin embargo, los archivos que se muestran aquí se vuelven TODOS a compilar si alguno de ellos se actualiza entre compilaciones.
+// No agregue aquí los archivos que se vayan a actualizar con frecuencia, ya que esto invalida la ventaja de rendimiento.
+
+#ifndef PCH_H
+#define PCH_H
+
+// agregue aquí los encabezados que desea precompilar
+#include "framework.h"
+
+#endif //PCH_H
