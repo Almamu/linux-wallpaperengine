df04be121ce8ce4970a31b4c88317f4468c3360f
Almamu <wpengine@alma.mu>
2026-03-22 23:21:39 +0100

    chore: background list test fix small issue with hasPreviews

--- tests/Tests/Library/Backgrounds.cpp
+++ tests/Tests/Library/Backgrounds.cpp
@@ -6,15 +6,17 @@
 
 #include "tests/Fixtures/BackgroundListFixture.h"
 
-#define REQUIRE_PATH_VALUES(root, entry_get, values, hasPreview) \
+#define REQUIRE_PATH_VALUES(root, entry_get, values, hasPreviews) \
 	{ \
 		wp_background_list_entry* entry = entry_get; \
 		REQUIRE (entry != nullptr); \
 		REQUIRE (entry->path != nullptr); \
-		if (hasPreview) { REQUIRE (entry->preview_path != nullptr); } else { REQUIRE (entry->preview_path == nullptr); } \
 		std::filesystem::path path = std::filesystem::path (entry->path).lexically_proximate(root); \
-		if (hasPreview) { \
+		if (hasPreviews.at (path)) { \
+			REQUIRE (entry->preview_path != nullptr); \
 			REQUIRE (std::string_view (entry->preview_path) == root / path / "preview.png"); \
+		} else { \
+			REQUIRE (entry->preview_path == nullptr); \
 		} \
 		REQUIRE (path.empty () == false); \
 		values.insert_or_assign (*path.begin(), true); \
@@ -33,16 +35,23 @@ TEST_CASE_METHOD (BackgroundListFixture, "Background list behaviour") {
 		{"100849", false}
 	};
 
+	std::map<std::string, bool> hasPreviews = {
+		{"3050", true},
+		{"5241", true},
+		{"9999", true},
+		{"100849", false}
+	};
+
 	wp_configuration* config = wp_config_create ();
 
 	REQUIRE(wp_config_set_backgrounds_dir (config, root.c_str ()) == true);
 
 	wp_background_list* list = wp_background_list_open (config);
 
-	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, false);
-	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, true);
-	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, true);
-	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, true);
+	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, hasPreviews);
+	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, hasPreviews);
+	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, hasPreviews);
+	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, hasPreviews);
 	// ensure all the values were found
 	std::ranges::for_each (values, [](auto& pair) -> void {
 		REQUIRE(pair.second == true);
@@ -54,7 +63,7 @@ TEST_CASE_METHOD (BackgroundListFixture, "Background list behaviour") {
 	// validate that after the last result we get a nullptr
 	REQUIRE(wp_background_list_next (list) == nullptr);
 	// but calling it again starts off the beginning again
-	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, false);
+	REQUIRE_PATH_VALUES(root, wp_background_list_next (list), values, hasPreviews);
 	REQUIRE(values.size () == 1);
 	REQUIRE(values.begin()->second == true);
 
